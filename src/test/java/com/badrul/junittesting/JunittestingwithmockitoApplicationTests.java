e89260264b3e648df09b21e3e60d17a39518d0af
rakibccj <rakibccj@gmail.com>
2022-08-23 22:32:09 +0600

    EmployeeController Test Case
    -- Display name added
    -- modify add employee test case due to service class mock

--- src/test/java/com/badrul/junittesting/JunittestingwithmockitoApplicationTests.java
+++ src/test/java/com/badrul/junittesting/JunittestingwithmockitoApplicationTests.java
@@ -3,8 +3,8 @@ package com.badrul.junittesting;
 import com.badrul.junittesting.entity.Employee;
 import com.badrul.junittesting.service.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,20 +40,26 @@ class JunittestingwithmockitoApplicationTests {
     }
 
     @Test
+    @DisplayName("Add Employee")
     void addEmployeeTest() throws Exception {
         Employee employee = Employee.builder().name("Rakib").email("rakibccj@gmail.com").mobile("01748141648").build();
+        Employee expectedEmployee = Employee.builder().id(1L).name("badrul").email("rakibccj@gmail.com").mobile("01748141648").build();
 
         String jsonRequest = om.writeValueAsString(employee);
 
+        when(employeeService.save(employee)).thenReturn(expectedEmployee);
         MvcResult result = mockMvc.perform(post("/employees").content(jsonRequest)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isCreated()).andReturn();
 
         String resultContent = result.getResponse().getContentAsString();
         Employee response = om.readValue(resultContent, Employee.class);
-        Assertions.assertTrue(response.getMobile().equalsIgnoreCase("017481416484"));
+        if (response.getName().equalsIgnoreCase("badrul"))
+            System.out.println("right ans");
+        assertEquals("017481416484", response.getMobile());
     }
 
     @Test
+    @DisplayName("Get Employee By Id")
     void getEmployeeByIdTest() throws Exception {
         long id = 1L;
         when(employeeService.getEmployeeById(id)).
@@ -69,10 +75,11 @@ class JunittestingwithmockitoApplicationTests {
     }
 
     @Test
+    @DisplayName("Get Employee List")
     void getEmployeesTest() throws Exception {
         when(employeeService.getEmployees())
                 .thenReturn(Stream.of(Employee.builder().id(1L).name("rezaul").mobile("01721-000000").build(),
-                                Employee.builder().id(1L).name("Karim").mobile("909090").build())
+                                Employee.builder().id(2L).name("Karim").mobile("909090").build())
                         .collect(Collectors.toList()));
 
         MvcResult result = mockMvc
